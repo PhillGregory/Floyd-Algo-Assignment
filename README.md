76eba8c679e1e77a367d51019aaed6aa6bdfdf23
Phillip Gregory <sgpgrego@liverpool.ac.uk>
2022-09-18 11:04:38 +0100

    Readme and requirements
    Completed Readme document. Included some known issues as I still can't get unit tests to work.
    Created requirements.txt document but this is blank, working to generate a complete requirements file

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Floyd-Algo-Assignment
-Write Floyd algorithm using recursive language
+
+## This project is a fully functional Floyd algorithm written recursively using Python
+
+The aim of this project is to take an iterative version of Floyd's algorithm, and rewrite it recursively.
+Floyd's algorithm is designed to find the shortest path between all vertices in a matrix. I used Visual Studio Code to create this project.
+
+This project includes:
+
+* An imperative solution to Floyd's algorithm
+* An iterative solution to Floyd's algorithm
+* A recursive solution to Floyd's algorithm
+* Unit tests to prove the algorithm's functionality
+* Performance tests to compare the efficiency of each version of the algorithm
+
+## Installation and usage instructions
+1. Clone this project using my repository link https://github.com/PhillipGregory/Floyd-Algo-Assignment.git
+2. Check and install any requirements using pip install -r requirements.txt
+3. Open the code for the desired algorithm in the FLoyd_Algorithms code
+4. Run the code directly on your chosen IDE
+
+## Known issues
+Unit tests are still showing errors. I am working to fix this so all algorithms pass their respective unit test.
+The goal of each unit test is to prove that an input matrix is equal to a known expected result after the algorithm has been run on the input
+
+
--- test_algos.py
+++ test_algos.py
@@ -1,5 +1,6 @@
 import unittest
 import Floyd_Algorithms.floyd_iterative_pdf
+import Floyd_Algorithms.floyd_iterative_geek
 import Floyd_Algorithms.floyd_recursive
 
 
@@ -10,21 +11,21 @@ expected_output = [[0, 7, 12, 8], [INF, 0, 5, 7], [INF, INF, 0, 2], [INF, INF, I
 
 
 class TestAlgo(unittest.TestCase):
-    input_graph = [[0, 7, INF, 8], [INF, 0, 5, INF], [INF, INF, 0, 2], [INF, INF, INF, 0]]
+    graph = [[0, 7, INF, 8], [INF, 0, 5, INF], [INF, INF, 0, 2], [INF, INF, INF, 0]]
 
     expected_output = [[0, 7, 12, 8], [INF, 0, 5, 7], [INF, INF, 0, 2], [INF, INF, INF, 0]]
 
     def test_iterative_pdf_algo(self):
-        result = Floyd_Algorithms.floyd_iterative_pdf.floyd_iter(self.input_graph)
+        result = Floyd_Algorithms.floyd_iterative_pdf.floyd_iter(self.graph)
         self.assertEqual(self.input_graph, result)
 
     def test_iterative_geek_algo(self):
-        result = Floyd_Algorithms.floyd_iter(self.input_graph)
-        self.assertEqual(self.input_graph, result)
+        result = Floyd_Algorithms.floyd_iterative_geek.floydWarshall(self.graph)
+        self.assertEqual(self.graph, result)
 
     #def test_recursive_algo(self):
-        #result = Floyd_Algorithms.floyd(self.k,i,j)
-        #self.assertEqual(self.input_graph, result)
+        #result = Floyd_Algorithms.floyd_recursive.sol(self,(0,0,0))
+        #self.assertEqual(self.graph, result)
         
 
 if __name__ == '__main__':
